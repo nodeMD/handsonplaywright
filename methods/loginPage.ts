9693f45a0a91afa8127c20c17849b231be4f0447
nodeMD <mdynieski@pm.me>
2022-06-27 10:27:25 +0200

    added login spec

---
+++ methods/loginPage.ts
@@ -0,0 +1,25 @@
+import { Page } from "@playwright/test";
+import { loginPageSelectors } from "../selectors/loginPage";
+
+export class LoginPage {
+  constructor(readonly page: Page) {
+    this.page = page;
+  }
+
+  async fillUsername(username: string) {
+    await this.page.locator(loginPageSelectors.inputs.username).fill("");
+    await this.page.locator(loginPageSelectors.inputs.username).fill(username);
+  }
+  async fillPassword(password: string) {
+    await this.page.locator(loginPageSelectors.inputs.password).fill("");
+    await this.page.locator(loginPageSelectors.inputs.password).fill(password);
+  }
+  async submitLoginForm() {
+    await this.page.locator(loginPageSelectors.buttons.logIn).click();
+  }
+  async fillAndSubmitLoginForm(username: any, password: any) {
+    await this.fillUsername(username);
+    await this.fillPassword(password);
+    await this.submitLoginForm();
+  }
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,41 @@
+import { test } from "@playwright/test";
+import { generateFakeUserData, validUser } from "../methods/generateData";
+import { LoginPage } from "../methods/loginPage";
+import { loginPageSelectors } from "../selectors/loginPage";
+
+const fakeUserData = generateFakeUserData();
+
+test.describe("login tests", () => {
+  let loginPage: LoginPage;
+  test.beforeEach(async ({ page }) => {
+    await page.goto("./");
+    loginPage = new LoginPage(page);
+  });
+
+  test("user should not be able to log in to app when using wrong login data", async ({
+    page,
+  }) => {
+    await loginPage.submitLoginForm();
+    await page.waitForSelector(
+      `text=${loginPageSelectors.validations.usernameIsRequired}`
+    );
+    await loginPage.fillUsername(fakeUserData.email.withoutAtSign);
+    await loginPage.submitLoginForm();
+    await page.waitForSelector(
+      `text=${loginPageSelectors.validations.passwordIsRequired}`
+    );
+    await loginPage.fillPassword(fakeUserData.password.onlyLowerCaseLetters);
+    await loginPage.submitLoginForm();
+    await page.waitForSelector(
+      `text=${loginPageSelectors.validations.userNotFound}`
+    );
+  });
+
+  test("user should be able to log in to app", async ({ page }) => {
+    await loginPage.fillAndSubmitLoginForm(
+      validUser.username,
+      validUser.password
+    );
+    await page.waitForURL("**/inventory.html");
+  });
+});
