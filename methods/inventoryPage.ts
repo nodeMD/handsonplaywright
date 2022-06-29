3e1f21934487d248e599abf7f81aec4120d049b6
nodeMD <mdynieski@pm.me>
2022-06-29 19:57:22 +0200

    [tests] added inventory page methods

--- methods/inventoryPage.ts
+++ methods/inventoryPage.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { inventoryPageSelectors } from "../selectors/inventoryPage";
 
 export class InventoryPage {
@@ -6,11 +6,32 @@ export class InventoryPage {
     this.page = page;
   }
 
+  async countInventoryItems() {
+    return await this.page.locator(inventoryPageSelectors.containers.item);
+  }
   async addItemToCart(itemNumber: number) {
     await this.page
       .locator(inventoryPageSelectors.buttons.addToCart)
       .nth(itemNumber)
       .click();
+  }
+  async checkThatRemoveFromCartButtonIsVisible() {
+    await this.page.waitForSelector(
+      `text=${inventoryPageSelectors.buttons.removeFromCart}`
+    );
+  }
+  async getNthItemPrice(itemNumber: number) {
+    return (
+      (await this.page
+        .locator(inventoryPageSelectors.containers.itemPrice)
+        .nth(itemNumber)
+        .textContent()) || ""
+    );
+  }
+  async checkThatCartIconShowsProperNumberOfItemsInCart(itemsInCart: number) {
+    await expect(
+      this.page.locator(inventoryPageSelectors.icons.shoppingCart)
+    ).toHaveText(itemsInCart.toString());
   }
   async openCart() {
     await this.page.locator(inventoryPageSelectors.buttons.cart).click();
