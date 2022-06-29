cf28e03682911ed5bbdb73a56ba068051c7309d8
nodeMD <mdynieski@pm.me>
2022-06-29 19:57:14 +0200

    [tests] added cart page methods

--- methods/cartPage.ts
+++ methods/cartPage.ts
@@ -1,9 +1,20 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { cartPageSelectors } from "../selectors/cartPage";
 
 export class CartPage {
   constructor(readonly page: Page) {
     this.page = page;
+  }
+
+  async checkThatNumberOfItemsInCartEquals(numberOfItems: number) {
+    await expect(
+      this.page.locator(cartPageSelectors.containers.cartItem)
+    ).toHaveCount(numberOfItems);
+  }
+  async checkThatCartItemPriceEquals(price: string) {
+    await expect(
+      this.page.locator(cartPageSelectors.containers.cartItemPrice)
+    ).toHaveText(price);
   }
   async clickCheckout() {
     await this.page.locator(cartPageSelectors.buttons.checkout).click();
