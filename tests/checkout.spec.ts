b21d40e9df6b51c64308e0a231bd58f5139a8f49
nodeMD <mdynieski@pm.me>
2022-06-29 19:57:43 +0200

    [tests] adjusted checkout e2e tests

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,22 +1,18 @@
 import { test, expect, Page } from "@playwright/test";
 import { CartPage } from "../methods/cartPage";
 import { CheckoutPage } from "../methods/checkoutPage";
+import { InventoryPage } from "../methods/inventoryPage";
+import { LoginPage } from "../methods/loginPage";
+import { checkoutPageSelectors } from "../selectors/checkoutPage";
 import {
   generateFakeUserData,
   validUser,
   randomItemNumberInRange,
 } from "../methods/generateData";
-import { InventoryPage } from "../methods/inventoryPage";
-import { LoginPage } from "../methods/loginPage";
-import { cartPageSelectors } from "../selectors/cartPage";
-import { checkoutPageSelectors } from "../selectors/checkoutPage";
-import { inventoryPageSelectors } from "../selectors/inventoryPage";
 
 const fakeUserData = generateFakeUserData();
 const desiredNumberOfItems = 6;
 const randomItem = randomItemNumberInRange(0, 5);
-const shippingMessage = "FREE PONY EXPRESS DELIVERY!";
-const thankYouMessage = "THANK YOU FOR YOUR ORDER";
 
 test.describe.serial("checkout tests", () => {
   let page: Page;
@@ -24,13 +20,14 @@ test.describe.serial("checkout tests", () => {
   let inventoryPage: InventoryPage;
   let cartPage: CartPage;
   let checkoutPage: CheckoutPage;
+
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage();
-    await page.goto("./");
     loginPage = new LoginPage(page);
     inventoryPage = new InventoryPage(page);
     cartPage = new CartPage(page);
     checkoutPage = new CheckoutPage(page);
+    await page.goto("./");
     await loginPage.fillAndSubmitLoginForm(
       validUser.username,
       validUser.password
@@ -39,33 +36,22 @@ test.describe.serial("checkout tests", () => {
   });
 
   test(`user is able to see list of products is which has length of ${desiredNumberOfItems} items`, async () => {
-    const numberOfItems = await page.locator(
-      inventoryPageSelectors.containers.item
-    );
+    const numberOfItems = await inventoryPage.countInventoryItems();
     await expect(numberOfItems).toHaveCount(desiredNumberOfItems);
   });
 
   test("user is able to add random item to cart then check if cart icon shows proper number and check if the proper price is shown on cart page", async () => {
     await inventoryPage.addItemToCart(randomItem);
-    await page.waitForSelector(
-      `text=${inventoryPageSelectors.buttons.removeFromCart}`
+    const numberOfItemsInCart = 1;
+    await inventoryPage.checkThatRemoveFromCartButtonIsVisible();
+    const price = await inventoryPage.getNthItemPrice(randomItem);
+    await inventoryPage.checkThatCartIconShowsProperNumberOfItemsInCart(
+      numberOfItemsInCart
     );
-    let price =
-      (await page
-        .locator(inventoryPageSelectors.containers.itemPrice)
-        .nth(randomItem)
-        .textContent()) || "";
-    await expect(
-      page.locator(inventoryPageSelectors.icons.shoppingCart)
-    ).toHaveText("1");
     await inventoryPage.openCart();
     await page.waitForURL("**/cart.html");
-    await expect(
-      page.locator(cartPageSelectors.containers.cartItem)
-    ).toHaveCount(1);
-    await expect(
-      page.locator(cartPageSelectors.containers.cartItemPrice)
-    ).toHaveText(price);
+    await cartPage.checkThatNumberOfItemsInCartEquals(numberOfItemsInCart);
+    await cartPage.checkThatCartItemPriceEquals(price);
   });
 
   test("user is able to proceed to checkout", async () => {
@@ -96,14 +82,16 @@ test.describe.serial("checkout tests", () => {
     await page.waitForURL("**/checkout-step-two.html");
   });
 
-  test(`user is able to see that the shipping information is '${shippingMessage}'`, async () => {
-    await page.waitForSelector(`text=${shippingMessage}`);
+  test(`user is able to see that the shipping information is '${checkoutPageSelectors.messages.shippingMessage}'`, async () => {
+    await page.waitForSelector(
+      `text=${checkoutPageSelectors.messages.shippingMessage}`
+    );
   });
 
-  test(`user is able to see confirmation message: '${thankYouMessage}'`, async () => {
+  test(`user is able to see confirmation message: '${checkoutPageSelectors.messages.thankYouMessage}'`, async () => {
     await checkoutPage.clickFinish();
     await expect(
       page.locator(checkoutPageSelectors.headers.complete)
-    ).toHaveText(thankYouMessage);
+    ).toHaveText(checkoutPageSelectors.messages.thankYouMessage);
   });
 });
