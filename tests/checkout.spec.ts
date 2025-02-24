26527b5e2be642fad4f03b2ca11759cab7232d47
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2025-02-24 08:42:19 +0100

    spec: add steps to checkout spec

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,27 +1,27 @@
 import { test, expect, Page } from "@playwright/test";
-import { CartPage } from "../methods/cartPage";
-import { CheckoutPage } from "../methods/checkoutPage";
-import { InventoryPage } from "../methods/inventoryPage";
-import { LoginPage } from "../methods/loginPage";
-import { checkoutPageSelectors } from "../selectors/checkoutPage";
+import { CartPage } from "../pages/cartPage/cart.page";
+import { CheckoutPage } from "../pages/checkoutPage/checkoutPage";
+import { InventoryPage } from "../pages/inventoryPage/inventory.page";
+import { LoginPage } from "../pages/loginPage/login.page";
+import { checkoutPageSelectors } from "../pages/checkoutPage/checkout.page.selectors";
 import {
   generateFakeUserData,
   validUser,
   randomItemNumberInRange,
-} from "../methods/generateData";
+} from "../utils/generateData";
 
 const fakeUserData = generateFakeUserData();
 const desiredNumberOfItems = 6;
 const randomItem = randomItemNumberInRange(0, 5);
 
-test.describe.serial("checkout tests", () => {
+test.describe("checkout tests", () => {
   let page: Page;
   let loginPage: LoginPage;
   let inventoryPage: InventoryPage;
   let cartPage: CartPage;
   let checkoutPage: CheckoutPage;
 
-  test.beforeAll(async ({ browser }) => {
+  test("user is able to finish checkout process", async ({ browser }) => {
     page = await browser.newPage();
     loginPage = new LoginPage(page);
     inventoryPage = new InventoryPage(page);
@@ -33,67 +33,67 @@ test.describe.serial("checkout tests", () => {
       validUser.password
     );
     await page.waitForURL("**/inventory.html");
-  });
 
-  test(`user is able to see list of products is which has length of ${desiredNumberOfItems} items`, async () => {
-    const numberOfItems = await inventoryPage.countInventoryItems();
-    await expect(numberOfItems).toHaveCount(desiredNumberOfItems);
-  });
+    await test.step(`user is able to see list of products is which has length of ${desiredNumberOfItems} items`, async () => {
+      const numberOfItems = await inventoryPage.countInventoryItems();
+      await expect(numberOfItems).toHaveCount(desiredNumberOfItems);
+    });
 
-  test("user is able to add random item to cart then check if cart icon shows proper number and check if the proper price is shown on cart page", async () => {
-    await inventoryPage.addItemToCart(randomItem);
-    const numberOfItemsInCart = 1;
-    await inventoryPage.checkThatRemoveFromCartButtonIsVisible();
-    const price = await inventoryPage.getNthItemPrice(randomItem);
-    await inventoryPage.checkThatCartIconShowsProperNumberOfItemsInCart(
-      numberOfItemsInCart
-    );
-    await inventoryPage.openCart();
-    await page.waitForURL("**/cart.html");
-    await cartPage.checkThatNumberOfItemsInCartEquals(numberOfItemsInCart);
-    await cartPage.checkThatCartItemPriceEquals(price);
-  });
+    await test.step("user is able to add random item to cart then check if cart icon shows proper number and check if the proper price is shown on cart page", async () => {
+      await inventoryPage.addItemToCart(randomItem);
+      const numberOfItemsInCart = 1;
+      await inventoryPage.checkThatRemoveFromCartButtonIsVisible();
+      const price = await inventoryPage.getNthItemPrice(randomItem);
+      await inventoryPage.checkThatCartIconShowsProperNumberOfItemsInCart(
+        numberOfItemsInCart
+      );
+      await inventoryPage.openCart();
+      await page.waitForURL("**/cart.html");
+      await cartPage.checkThatNumberOfItemsInCartEquals(numberOfItemsInCart);
+      await cartPage.checkThatCartItemPriceEquals(price);
+    });
 
-  test("user is able to proceed to checkout", async () => {
-    await cartPage.clickCheckout();
-    await page.waitForURL("**/checkout-step-one.html");
-  });
+    await test.step("user is able to proceed to checkout", async () => {
+      await cartPage.clickCheckout();
+      await page.waitForURL("**/checkout-step-one.html");
+    });
 
-  test("user is unable to continue without filling checkout form with all required data", async () => {
-    await checkoutPage.clickContinue();
-    await checkoutPage.checkValidation(
-      checkoutPageSelectors.validations.firstNameIsRequired
-    );
-    await checkoutPage.fillFirstName(fakeUserData.name);
-    await checkoutPage.clickContinue();
-    await checkoutPage.checkValidation(
-      checkoutPageSelectors.validations.lastNameIsRequired
-    );
-    await checkoutPage.fillLastName(fakeUserData.lastName);
-    await checkoutPage.clickContinue();
-    await checkoutPage.checkValidation(
-      checkoutPageSelectors.validations.postalCodeIsRequired
-    );
-  });
+    await test.step("user is unable to continue without filling checkout form with all required data", async () => {
+      await checkoutPage.clickContinue();
+      await checkoutPage.checkValidation(
+        checkoutPageSelectors.validations.firstNameIsRequired
+      );
+      await checkoutPage.fillFirstName(fakeUserData.name);
+      await checkoutPage.clickContinue();
+      await checkoutPage.checkValidation(
+        checkoutPageSelectors.validations.lastNameIsRequired
+      );
+      await checkoutPage.fillLastName(fakeUserData.lastName);
+      await checkoutPage.clickContinue();
+      await checkoutPage.checkValidation(
+        checkoutPageSelectors.validations.postalCodeIsRequired
+      );
+    });
 
-  test("user is able to continue after filling checkout form with required data", async () => {
-    await checkoutPage.fillPostalCode(fakeUserData.postalCode);
-    await checkoutPage.clickContinue();
-    await page.waitForURL("**/checkout-step-two.html");
-  });
+    await test.step("user is able to continue after filling checkout form with required data", async () => {
+      await checkoutPage.fillPostalCode(fakeUserData.postalCode);
+      await checkoutPage.clickContinue();
+      await page.waitForURL("**/checkout-step-two.html");
+    });
 
-  test(`user is able to see that the shipping information is '${checkoutPageSelectors.messages.shippingMessage}'`, async () => {
-    await checkoutPage.checkMessageOnSelector(
-      checkoutPageSelectors.labels.deliverySummary,
-      checkoutPageSelectors.messages.shippingMessage
-    );
-  });
+    await test.step(`user is able to see that the shipping information is '${checkoutPageSelectors.messages.shippingMessage}'`, async () => {
+      await checkoutPage.checkMessageOnSelector(
+        checkoutPageSelectors.labels.deliverySummary,
+        checkoutPageSelectors.messages.shippingMessage
+      );
+    });
 
-  test(`user is able to see confirmation message: '${checkoutPageSelectors.messages.thankYouMessage}'`, async () => {
-    await checkoutPage.clickFinish();
-    await checkoutPage.checkMessageOnSelector(
-      checkoutPageSelectors.headers.complete,
-      checkoutPageSelectors.messages.thankYouMessage
-    );
+    await test.step(`user is able to see confirmation message: '${checkoutPageSelectors.messages.thankYouMessage}'`, async () => {
+      await checkoutPage.clickFinish();
+      await checkoutPage.checkMessageOnSelector(
+        checkoutPageSelectors.headers.complete,
+        checkoutPageSelectors.messages.thankYouMessage
+      );
+    });
   });
 });
